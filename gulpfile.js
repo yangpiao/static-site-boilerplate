dc89b708fd6d9d02bd614d80eb01a4866b43cc8a
Yang Piao <yp@yangpiao.net>
2019-07-12 00:18:23 -0700

    Upgraded all dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,71 @@
-const gulp = require('gulp');
-const exec = require('child_process').exec;
-const sass = require('node-sass');
+const { src, dest, series, parallel, watch } = require('gulp');
+const { exec } = require('child_process');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const browserSync = require('browser-sync').create();
+
 const properties = require('./properties');
-const allFiles = '/**/*';
 const glob = {
-  posts: properties.dir.posts + allFiles + '.md',
-  pages: properties.dir.pages + allFiles + '.md',
-  templates: properties.dir.templates + allFiles + '.{html,xml}',
-  static: properties.dir.static + allFiles,
-  styles: properties.dir.styles + allFiles + '.scss',
-  css: properties.dir.css + allFiles + '.css'
+  posts: `${properties.dir.posts}/**/*.md`,
+  pages: `${properties.dir.pages}/**/*.md`,
+  templates: `${properties.dir.templates}/**/*.{html,xml}`,
+  static: `${properties.dir.static}/**/*`,
+  styles: `${properties.dir.styles}/**/*.scss`,
+  css: `${properties.dir.css}/**/*.css`
 };
-
-gulp.task('clean', callback => {
-  exec('rm -rf ' + properties.dir.build, callback);
-});
-
-gulp.task('sass', callback => {
-  exec(properties.sass.join(' '), callback);
-});
-
-gulp.task('build:src', callback => {
-  exec(properties.builder.join(' '), callback);
-});
-
-gulp.task('build:static', () =>
-  gulp.src(glob.static)
-    .pipe(gulp.dest(properties.dir.build))
-);
-
-gulp.task('styles', ['sass'], () =>
-  gulp.src(glob.css)
-    .pipe(postcss([
-      autoprefixer({ browsers: ['last 5 versions', 'not ie <= 8'] })
-    ]))
-    .pipe(gulp.dest(properties.dir.css))
-);
-
-gulp.task('build', ['build:src', 'build:static', 'styles']);
-gulp.task('default', ['clean', 'build']);
-
-const reload = callback => {
-  browserSync.reload();
-  callback();
-};
-gulp.task('watch:styles', ['styles'], reload);
-gulp.task('watch:src', ['build:src'], reload);
-gulp.task('watch:static', ['build:static'], reload);
-
-gulp.task('serve', ['default'], () => {
-  browserSync.init({
-    server: {
-      baseDir: properties.dir.build
-    }
-  });
-  gulp.watch([
+const watchedFiles = {
+  src: [
     glob.posts,
     glob.pages,
     glob.templates,
     './site.config.yml',
     './lib/**/*'
-  ], ['watch:src']);
-  gulp.watch(glob.static, ['watch:static']);
-  gulp.watch(glob.styles, ['watch:styles']);
+  ],
+  static: glob.static,
+  styles: glob.styles
+};
+
+// >>>>>>>>>> tasks <<<<<<<<<<
+
+const clean = callback => exec(`rm -rf ${properties.dir.build}`, callback);
+
+const buildSrc = callback => exec(properties.builder.join(' '), callback);
+
+const buildStatic = () => src(glob.static).pipe(dest(properties.dir.build));
+
+const buildSass = callback => exec(properties.sass.join(' '), callback);
+const buildPostcss = () =>
+  src(glob.css)
+    .pipe(postcss([ autoprefixer() ]))
+    .pipe(dest(properties.dir.css));
+const buildStyles = series(buildSass, buildPostcss);
+
+const build = parallel(buildSrc, buildStatic, buildStyles);
+
+const reload = callback => {
+  browserSync.reload();
+  callback();
+};
+
+const serve = () => {
+  browserSync.init({
+    server: {
+      baseDir: properties.dir.build
+    }
+  });
+
+  watch(watchedFiles.src, series(buildSrc, reload));
+  watch(watchedFiles.static, series(buildStatic, reload));
+  watch(watchedFiles.styles, series(buildStyles, reload));
+};
+
+Object.assign(exports, {
+  clean,
+  buildSrc,
+  buildStatic,
+  buildStyles,
+  build,
+  serve
 });
+exports.default = series(clean, build);
+
